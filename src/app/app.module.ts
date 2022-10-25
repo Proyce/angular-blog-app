d0c73b9794303a3a08ea8f8a5d1a6a0fdb4240ca
proyce <prospernnadi3@gmail.com>
2022-10-25 23:36:58 +0100

    Added Blog List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule
